e56b29399fcdbb194eb14562440f46ccde7be8f9
ToF- <cthibaut@octo.com>
2015-08-17 11:52:30 +0200

    ask questions and keep the responses with delay for each response

--- Spike.hs
+++ Spike.hs
@@ -1,11 +1,24 @@
 
 import System.Time
 
-main = do
+type Question = (String,String)
+type Response = (String,Delay)
+type Delay = Int
+
+ask :: Question -> IO Response
+ask (q,_) = do
+    putStrLn q
     t0 <- getClockTime
     l <- getLine
-    t1 <- getClockTime 
-    let d = diffClockTimes t1  t0
-    putStrLn $ show $ (l,tdSec d)
-    main
+    t1 <- getClockTime
+    return (l,tdSec $ diffClockTimes t1 t0)
+
+quizz :: [Question] -> IO [Response] 
+quizz [] = return []
+quizz (q:qs) = do
+    r <- ask q
+    rs <- quizz qs
+    return (r:rs)
+
+main = quizz $ map (\n -> ("mnemo pour le nombre: "++ show n,"idk")) [0..3] 
 
